c9a2bdf87d42dc265f1c6933eed46bb6dc5190f6
fabT <fabien.varlet@live.fr>
2022-11-25 17:22:21 +0100

    Mise en place Routes

---
+++ src/pages/LodgingSheet/index.jsx
@@ -0,0 +1,11 @@
+function LodgingSheet() {
+  return (
+    <div className="LodgingSheet">
+      <header className="LodgingSheet-header">
+        <h1>LodgingSheet</h1>
+      </header>
+    </div>
+  )
+}
+
+export default LodgingSheet
