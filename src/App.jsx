daf9b10c89334e23bbc48e4e45afe2e456f654eb
fabT <fabien.varlet@live.fr>
2022-11-25 18:26:08 +0100

    Mise en place Routes OK

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import LodgingSheet from './pages/LodgingSheet'
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/lodging-sheet" element={<LodgingSheet />} />
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/lodging-sheet" element={<LodgingSheet />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
