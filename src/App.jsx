765f4da27646bbc8e1327d871df7c5433f004ac2
fabT <fabien.varlet@live.fr>
2023-01-02 12:03:41 +0100

    nettoyage code 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
 import Home from './pages/Home'
 import About from './pages/About'
 import LodgingSheet from './pages/LodgingSheet'
 import Error from './components/Error'
-import Header from './components/Header'
-import Footer from './components/Footer'
-// import { PageSelectedProvider } from './utils/context'
 
 function App() {
   return (
     <div className="app">
       <BrowserRouter>
-        {/* <PageSelectedProvider> */}
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
@@ -21,7 +19,6 @@ function App() {
           <Route path="/*" element={<Error />} />
         </Routes>
         <Footer />
-        {/* </PageSelectedProvider> */}
       </BrowserRouter>
     </div>
   )
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -5,14 +5,12 @@ function Navbar() {
     <nav className="navBar">
       <NavLink
         to="/"
-        // end
         className={({ isActive }) => `navBarLink ${isActive && 'active'}`}
       >
         Accueil
       </NavLink>
       <NavLink
         to="/about"
-        // end
         className={({ isActive }) => `navBarLink ${isActive && 'active'}`}
       >
         A propos
